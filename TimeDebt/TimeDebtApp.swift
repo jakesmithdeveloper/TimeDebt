aed3f01cedd47ca26d7a9cfff432057a56350ac9
Jake Smith <33109702+jakesmithdeveloper@users.noreply.github.com>
2024-04-02 10:15:38 -0400

    Initial Commit

---
+++ TimeDebt/TimeDebtApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeDebtApp.swift
+//  TimeDebt
+//
+//  Created by Jake Smith on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeDebtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
